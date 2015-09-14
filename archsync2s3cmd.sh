31dbecae36722c222c47b1b8323af465f8ed692f
MaliciaMrg <malicia_mrg@hotmail.com>
2015-09-14 22:35:43 +0200

    20150914

--- archsync2s3cmd.sh
+++ archsync2s3cmd.sh
@@ -1,19 +1,39 @@
-#!/bin/bash
-echo "$(date +"%F %T") start" >> "./log/${1}-log-`date +%Y-%m-%d`.txt"
-s3cmd sync /media/$1/ -p -r -v --delete-removed --dry-run s3://malicia-warehouse-$1/ > dryrunout.txt
-nbdel=$(grep -w "delete" -c dryrunout.txt)
-nbupl=$(grep -w "upload" -c dryrunout.txt)
-nbfil=$(find /media/$1/ -type f | wc -l)
-percent=0.01
-nbfillvl=$(echo $percent $nbfil | awk '{printf "%4d\n",$1*$2}')
-echo "${1} - $(date +"%F %T") nb upload to aws = $nbupl / nb del in aws = $nbdel / threshold del pour cancel = $nbfillvl / nb fichier repetoire = $nbfil" >> archsync2s3cmd.log
-if [ "$nbdel" -gt "$nbfillvl" ]
+#!/usr/bin/env bash
+
+exec 9<$0
+flock -n 9 || exit 1
+
+dtl=`date +%Y-%m-%d`
+echo "$(date +"%F %T") start" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+
+
+FILE=s3cmdparam.ini
+exec 3<&0
+exec 0<$FILE
+while read line 
+do 
+	word=($(echo $line| tr ':' ' ' | sed 's/\r/ /g'))
+	param1=${word[0]}
+	param2=${word[1]}
+	echo $param1 " - " $param2 >> "./log/archsync2s3cmd-log-${dtl}.txt"
+	echo $param1 " - " $param2 >> "./log/archsync2s3cmd-log-${dtl}-s3cmd.txt"
+	rm dryrunout.txt
+	s3cmd sync $param1/$param2/ -p -r -v --delete-removed --dry-run s3://malicia-warehouse-$param2/ > dryrunout.txt
+	nbdel=$(grep -w "delete" -c dryrunout.txt)
+	nbupl=$(grep -w "upload" -c dryrunout.txt)
+	nbfil=$(find $param1/$param2/ -type f | wc -l)
+	percent=0.01
+	nbfillvl=$(echo $percent $nbfil | awk '{printf "%4d\n",$param1*$param2}')
+	echo "${param2} - $(date +"%F %T") nb upload to aws = $nbupl / nb del in aws = $nbdel / threshold del pour cancel = $nbfillvl / nb fichier repetoire = $nbfil"  >> "./log/archsync2s3cmd-log-${dtl}.txt"
+if $nbdel > $nbfillvl
 then
-	echo "-- trop de delete on stop --" >>  "./log/${1}-log-`date +%Y-%m-%d`.txt"
-	echo "${1} - $(date +"%F %T") not done threshold delete" >> archsync2s3cmd.log
-else
-	s3cmd sync /media/$1/ -p -r -v --delete-removed s3://malicia-warehouse-$1/ >> "./log/${1}-log-`date +%Y-%m-%d`.txt"
-	echo "${1} - $(date +"%F %T") done" >> archsync2s3cmd.log
-fi
-echo "$(date +"%F %T") stop " >>  "./log/${1}-log-`date +%Y-%m-%d`.txt"
+		echo "-- trop de delete on stop --" >> "./log/archsync2s3cmd-log-${dtl}-s3cmd.txt"
+		echo "${param2} - $(date +"%F %T") not done threshold delete" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+	else
+		s3cmd sync $param1/$param2/ -p -r -v --delete-removed s3://malicia-warehouse-$param2/ >> "./log/archsync2s3cmd-log-${dtl}-s3cmd.txt"
+		echo "${param2} - $(date +"%F %T") done" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+	fi
+done 
+exec 0<&3
+echo "$(date +"%F %T") stop "  >> "./log/archsync2s3cmd-log-${dtl}.txt"
 
