88dae80a1e75d5b184e0e5f4624c74cc4daa39d2
MaliciaMrg <malicia_mrg@hotmail.com>
2015-12-08 00:20:20 +0100

    archtest numerique

--- archsync2s3cmd.sh
+++ archsync2s3cmd.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 dtl=`date +%Y-%m-%d`
-echo "$(date +"%F %T") archsync2s3cmd.sh v2" >> "./log/archsync2s3cmd-log-${dtl}.txt"
-echo "$(date +"%F %T") start" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+logger_cmd="logger  -p local6.debug -t $0[$$]"
+tmpfile="~/tmp/s3cmdbatch-sync.txt"
+$logger_cmd "start"
 (
     function quit {
-    	file=$(ls -t ./log/archsync2s3cmd-log-*-s3cmd.txt | head -1)
-        filestring=$(tail -n1 $file)
-        numd=$((`expr index "$filestring" :` + 2))
-        lend=$((`expr index "${filestring:$numd}" /` - 1))
-        numf=$((`expr index "$filestring" [` - 1))
-        tweet "archsync2s3cmd : backup en cours ${filestring:$numf} sur ${filestring:$numd:lend}"
+        if [ -f $tmpfile ]; then
+          file=$(ls -t "$tmpfile" | head -1)
+          filestring=$(tail -n1 $file)
+          numd=$((`expr index "$filestring" :` + 2))
+          lend=$((`expr index "${filestring:$numd}" /` - 1))
+          numf=$((`expr index "$filestring" [` - 1))
+          tweet "archsync2s3cmd : backup en cours ${filestring:$numf} sur ${filestring:$numd:lend}"
+        fi
     	exit 1
     }
     flock -n 200 || quit;
-	echo "$(date +"%F %T") locked" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+    $logger_cmd "locked"
 	FILE=s3cmdparam.ini
 	exec 0<$FILE
 	while read line <&0
@@ -21,8 +24,7 @@ echo "$(date +"%F %T") start" >> "./log/archsync2s3cmd-log-${dtl}.txt"
 		word=($(echo $line| tr ':' ' ' | sed 's/\r/ /g'))
 		param1=${word[0]}
 		param2=${word[1]}
-		echo $param1 " - " $param2 >> "./log/archsync2s3cmd-log-${dtl}.txt"
-		echo $param1 " - " $param2 >> "./log/archsync2s3cmd-log-${dtl}-s3cmd.txt"
+		$logger_cmd "${param2} <- $param1"
 		rm dryrunout-$param2.txt
 		sleep 5
 		s3cmd sync $param1/$param2/ -p -r -v --delete-removed --dry-run s3://malicia-warehouse-$param2/ > dryrunout-$param2.txt
@@ -30,26 +32,25 @@ echo "$(date +"%F %T") start" >> "./log/archsync2s3cmd-log-${dtl}.txt"
 		nbdel=$(grep -w "delete" -c dryrunout-$param2.txt)
 		nbupl=$(grep -w "upload" -c dryrunout-$param2.txt)
 		nbfil=$(find $param1/$param2/ -type f | wc -l)
-		percent=0.01
-		nbfillvl=$(echo $percent $nbfil | awk '{printf "%4d\n",$param1*$param2}')
-		echo "${param2} - $(date +"%F %T") nb upload to aws = $nbupl / nb del in aws = $nbdel / threshold del pour cancel = $nbfillvl / nb fichier repetoire = $nbfil"  >> "./log/archsync2s3cmd-log-${dtl}.txt"
+		percent="0.01"
+		#nbfillvl=$(echo $percent $nbfil | awk '{printf "%4d\n",$param1*$param2}')
+        nbfillvl=($(echo "${nbfil} * ${percent}" | bc -l))
+		$logger_cmd "${param2} nb upload to aws = ${nbupl} / nb del in aws = ${nbdel} / threshold del pour cancel = ${nbfillvl} / nb fichier repetoire = ${nbfil}"
 		if (($nbupl <= 0)) ;	then
-			echo "-- rien a trasferet on stop --" >> "./log/archsync2s3cmd-log-${dtl}-s3cmd.txt"
-			echo "${param2} - $(date +"%F %T") nothing " >> "./log/archsync2s3cmd-log-${dtl}.txt"
+			 $logger_cmd "${param2} nothing "
 		else
 			if (($nbdel > $nbfillvl)) ;	then
-				echo "-- trop de delete on stop --" >> "./log/archsync2s3cmd-log-${dtl}-s3cmd.txt"
-				echo "${param2} - $(date +"%F %T") not done threshold delete" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+				$logger_cmd "${param2} not done threshold delete"
             	tweet "archsync2s3cmd : trop de delete pour $param2 => $nbdel (nbdel) ,  $nbfillvl (threshold) "
 			else
             	tweet "archsync2s3cmd : lancement backup pour $param2 => $nbupl (nbupload) , $nbdel (nbdel) "
-				s3cmd sync $param1/$param2/ -p -r -v --delete-removed s3://malicia-warehouse-$param2/ >> "./log/archsync2s3cmd-log-${dtl}-s3cmd.txt"  2>&1
-				echo "${param2} - $(date +"%F %T") done" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+				s3cmd sync $param1/$param2/ -p -r -v --delete-removed s3://malicia-warehouse-$param2/ >> $tmpfile  2>&1
+				$logger_cmd "${param2} done"
 				tweet "archsync2s3cmd : fin backup pour $param2"
 			fi;
 		fi;
 	done 
 	exec 0<&-
-	echo "$(date +"%F %T") unlocked "  >> "./log/archsync2s3cmd-log-${dtl}.txt"
+	$logger_cmd "unlocked"
 ) 200>archsync2s3cmd.lock
-echo "$(date +"%F %T") stop" >> "./log/archsync2s3cmd-log-${dtl}.txt"
+ $logger_cmd "stop"
--- testarchsync2s3cmd.sh
+++ testarchsync2s3cmd.sh
@@ -1,39 +1,56 @@
 #!/usr/bin/env bash
 dtl=`date +%Y-%m-%d`
-echo "$(date +"%F %T") archsync2s3cmd.sh v2" 
-echo "$(date +"%F %T") start"
-	echo "$(date +"%F %T") locked"
-	FILE=tests3cmdparam.ini
+logger_cmd="echo $0[$$]"
+tmpfile="~/tmp/s3cmdbatch-sync.txt"
+$logger_cmd "start"
+(
+    function quit {
+        if [ -f $tmpfile ]; then
+          file=$(ls -t "$tmpfile" | head -1)
+          filestring=$(tail -n1 $file)
+          numd=$((`expr index "$filestring" :` + 2))
+          lend=$((`expr index "${filestring:$numd}" /` - 1))
+          numf=$((`expr index "$filestring" [` - 1))
+          $logger_cmd "archsync2s3cmd : backup en cours ${filestring:$numf} sur ${filestring:$numd:lend}"
+        fi
+    	exit 1
+    }
+    flock -n 200 || quit;
+    $logger_cmd "locked"
+	FILE=s3cmdparam.ini
 	exec 0<$FILE
 	while read line <&0
 	do 
 		word=($(echo $line| tr ':' ' ' | sed 's/\r/ /g'))
 		param1=${word[0]}
 		param2=${word[1]}
-		echo $param1 " - " $param2 
-		echo $param1 " - " $param2 
-		rm testdryrunout-$param2.txt
+		$logger_cmd "${param2} <- $param1"
+		rm dryrunout-$param2.txt
 		sleep 5
-		s3cmd --progress sync $param1/$param2/ -p -r -v --delete-removed --dry-run s3://malicia-warehouse-$param2/ > testdryrunout-$param2.txt 2>&1
-		#exec('s3cmd --progress sync $param1/$param2/ -p -r -v --delete-removed --dry-run s3://malicia-warehouse-$param2/  2>&1', testdryrunout-$param2.txt); 
+		#s3cmd sync $param1/$param2/ -p -r -v --delete-removed --dry-run s3://malicia-warehouse-$param2/ > dryrunout-$param2.txt
 		sleep 5
-		nbdel=$(grep -w "delete" -c testdryrunout-$param2.txt)
-		nbupl=$(grep -w "upload" -c testdryrunout-$param2.txt)
+		nbdel=$(grep -w "delete" -c dryrunout-$param2.txt)
+		nbupl=$(grep -w "upload" -c dryrunout-$param2.txt)
 		nbfil=$(find $param1/$param2/ -type f | wc -l)
-		percent=0.01
-		nbfillvl=$(echo $percent $nbfil | awk '{printf "%4d\n",$param1*$param2}')
-		echo "${param2} - $(date +"%F %T") nb upload to aws = $nbupl / nb del in aws = $nbdel / threshold del pour cancel = $nbfillvl / nb fichier repetoire = $nbfil"  
+		percent="0.01"
+		#nbfillvl=$(echo $percent $nbfil | awk '{printf "%4d\n",$param1*$param2}')
+        nbfillvl=($(echo "${nbfil} * ${percent}" | bc -l))
+		$logger_cmd "${param2} nb upload to aws = ${nbupl} / nb del in aws = ${nbdel} / threshold del pour cancel = ${nbfillvl} / nb fichier repetoire = ${nbfil}"
 		if (($nbupl <= 0)) ;	then
-			echo "-- rien a trasferet on stop --"
-			echo "${param2} - $(date +"%F %T") nothing " 
+			 $logger_cmd "${param2} nothing "
 		else
 			if (($nbdel > $nbfillvl)) ;	then
-				echo "-- trop de delete on stop --" 
-				echo "${param2} - $(date +"%F %T") not done threshold delete" 
+				$logger_cmd "${param2} not done threshold delete"
+            	$logger_cmd "archsync2s3cmd : trop de delete pour $param2 => $nbdel (nbdel) ,  $nbfillvl (threshold) "
 			else
-				echo "${param2} - $(date +"%F %T") done" 
+            	$logger_cmd "archsync2s3cmd : lancement backup pour $param2 => $nbupl (nbupload) , $nbdel (nbdel) "
+				#s3cmd sync $param1/$param2/ -p -r -v --delete-removed s3://malicia-warehouse-$param2/ >> $tmpfile  2>&1
+				$logger_cmd "${param2} done"
+				$logger_cmd "archsync2s3cmd : fin backup pour $param2"
 			fi;
 		fi;
 	done 
-
-echo "$(date +"%F %T") stop"
+	exec 0<&-
+	$logger_cmd "unlocked"
+) 200>testarchsync2s3cmd.lock
+ $logger_cmd "stop"
